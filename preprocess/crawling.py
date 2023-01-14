faeb560ae23c2bbd4ee918deca22e4c1d5563a24
sihyeong671 <bshlab671@naver.com>
2023-01-14 21:43:40 +0900

    fix crawling to multi thread, processing

--- preprocess/crawling.py
+++ preprocess/crawling.py
@@ -1,81 +1,124 @@
 import os
+import sys
+from time import sleep, time
+from tqdm import tqdm
+import urllib.request
 import requests as rq
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
+from multiprocessing import Pool
+from concurrent.futures.thread import ThreadPoolExecutor
+import concurrent.futures
+
 from bs4 import BeautifulSoup
-from time import sleep
-import urllib.request
-import sys
-from tqdm import tqdm
+
+DATA_PATH = "../data/crawed_img/"
 
 # reference : https://kimcoder.tistory.com/259
 # brew install --cask chromedriver
 # sudo mv chormedriver /usr/local/bin
+    
 
-DATA_PATH = "./crawed_img/"
+def get_img_urls(keyword):
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless') # 창 띄우지 않고 실행
+    options.add_argument('--disable-gpu') # gpu 사용 x
+    options.add_argument('lang=ko_KR') # 언어 한글
+    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+    options.add_argument(f"user-agent={user_agent}")
+    service = Service(executable_path="/usr/local/bin/chormedriver")
 
-keywords = ['indoor', 'child', 'table']
-
-os.makedirs(DATA_PATH, exist_ok=True)
-for k in keywords:
-    os.makedirs(os.path.join(DATA_PATH, k), exist_ok=True)
-
-options = webdriver.ChromeOptions()
-options.add_argument('headless') # 창 띄우지 않고 실행
-options.add_argument('--disable-gpu') # gpu 사용 x
-options.add_argument('lang=ko_KR') # 언어 한글
-user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
-options.add_argument(f"user-agent={user_agent}")
-service = Service(executable_path="/usr/local/bin/chormedriver")
-
-with webdriver.Chrome(service=service, options=options) as browser:
-    count = 0
-    for keyword in keywords:
+    img_urls = []
+    with webdriver.Chrome(service=service, options=options) as browser:
+        # 전체 페이지 갯수 구하기
         pixabay_url = f"https://pixabay.com/ko/images/search/{keyword}"
         browser.get(pixabay_url)
-        sleep(.1)
+        sleep(.5)
         html = browser.page_source
         soup = BeautifulSoup(html,'html.parser')
         text = soup.find('form', 'add_search_params pure-form').getText()
+        text = text.replace('/', '')
+        start_idx = 0
+        end_idx = 0
+        for idx, t in enumerate(text):
+            if t.isdigit() and start_idx == 0:
+                start_idx = idx
+            if start_idx != 0 and not t.isdigit():
+                end_idx = idx
         try:
-            pages = int(text[5:7])
+            pages = int(text[start_idx:end_idx])
         except:
-            ValueError(f'ERROR(pages : {pages})')
-            
+            ValueError('ERROR(pages)')
+        # img_urls 반환
         for page in tqdm(range(1, pages+1)):
             pixabay_url = f"https://pixabay.com/ko/images/search/{keyword}/?pagi={page}"
             browser.get(pixabay_url)
-            sleep(.1)
+            sleep(.5)
             html = browser.page_source
             soup = BeautifulSoup(html,'html.parser')
-            imgs = soup.select('div.row-masonry.search-results img')
+            urls = soup.select('div.row-masonry.search-results img')
+            for url in urls:
+                img_urls.append(url)
+    return img_urls
+
+def get_img_and_save(img_url, keyword):
+    srcset = ""
+    if img_url.get('srcset') == None:
+        srcset = img_url.get('data-lazy-srcset')
+    else: 
+        srcset = img_url.get('srcset')
+        
+    src = ""
+    if len(srcset):
+        src = str(srcset).split()[2] # 480 크기 이미지 가져옴 (기본 340)
+        filename = src.split('/')[-1] #이미지 경로에서 날짜 부분뒤의 순 파일명만 추출
+        saveUrl = os.path.join(DATA_PATH, keyword, filename) # 저장 경로 결정
+
+        #파일 저장
+        #user-agent 헤더를 가지고 있어야 접근 허용하는 사이트도 있을 수 있음(pixabay가 이에 해당)
+        try:
+            headers = {'User-Agent': 'Mozilla/5.0'} 
+            timeout = 5
+            img = rq.get(src, headers=headers, timeout=timeout)
+            if img.status_code == 200:
+                with open(saveUrl, "wb") as f: #디렉토리 오픈
+                    f.write(img.content) #파일 저장
+            else:
+                print('status code error')
+        except Exception as e:
+            print(e)
+            sys.exit(0)
+
+def thread_crawling(keyword):
+    thread_list = []
+    img_urls = get_img_urls(keyword)
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        for img_url in img_urls:
+            thread_list.append(executor.submit(get_img_and_save, img_url, keyword))
+        for execution in concurrent.futures.as_completed(thread_list):
+            execution.result()
             
-            for img in imgs:
-                count += 1
-                srcset = ""
-                if img.get('srcset') == None:
-                    srcset = img.get('data-lazy-srcset')
-                else: 
-                    srcset = img.get('srcset')
-                    
-                src = ""
-                if len(srcset):
-                    src = str(srcset).split()[2] # 480 크기 이미지 가져옴 (기본 340)
-                    filename = src.split('/')[-1] #이미지 경로에서 날짜 부분뒤의 순 파일명만 추출
-                    saveUrl = os.path.join(DATA_PATH, keyword, filename) #저장 경로 결정
+def do_process_with_thread_crawl(keyword: str):
+    thread_crawling(keyword)
 
-                    #파일 저장
-                    #user-agent 헤더를 가지고 있어야 접근 허용하는 사이트도 있을 수 있음(pixabay가 이에 해당)
-                    req = urllib.request.Request(src, headers={'User-Agent': 'Mozilla/5.0'})
-                    try:
-                        imgUrl = urllib.request.urlopen(req).read() #웹 페이지 상의 이미지를 불러옴
-                        with open(saveUrl, "wb") as f: #디렉토리 오픈
-                            f.write(imgUrl) #파일 저장
-                    except urllib.error.HTTPError:
-                        print('에러')
-                        sys.exit(0)
+    
+if __name__ == "__main__":
+    
+    keywords = ['child', 'table', 'indoor']
 
-print(f"전체 이미지 갯수:{count}")
-print("FIN")
+    os.makedirs(DATA_PATH, exist_ok=True)
+    for k in keywords:
+        os.makedirs(os.path.join(DATA_PATH, k), exist_ok=True)
+        
+    start_time = time()
+    
+    with Pool(processes=3) as pool: # keyword마다 프로세스 할당
+        pool.map(do_process_with_thread_crawl, keywords)
+
+    end_time = time()
+    
+    print(f"소요시간: {end_time - start_time}")
+    
--- preprocess/merge_bg.py
+++ preprocess/merge_bg.py
@@ -5,38 +5,41 @@ from glob import glob
 from tqdm import tqdm
 
 data_dir = "../data/train"
-background_dir = "../data/crawed_img/child/"
-background_dir_2 = "../data/crawed_img/indoor/"
+bg_dir = "../data/crawed_img"
+
+keywords = ['indoor', 'table', 'child']
 
 img_list = sorted(glob(os.path.join(data_dir, "*.jpg")))
-background_list = glob(os.path.join(background_dir,"*.jpg"))
-background_list_2 = glob(os.path.join(background_dir_2,"*.jpg"))
 
-## background_list, img_list 개수 맞추기
-background_list += background_list_2 + background_list_2 + background_list_2 + background_list_2[:2635]
+bg_list = []
+for k in keywords:
+    bg_list.extend(glob(os.path.join(bg_dir, k)))
 
-for img_l, background in tqdm(zip(img_list,background_list)):
-        # './train\\TRAIN_00999.jpg'
-        name = img_l[-15:]
-        img = cv2.imread(img_l)
 
-        thresh = 230
-        
-        img_bin = np.array(img[:, :, 0] < thresh, dtype=np.uint8) * 255
-        img_blur = cv2.medianBlur(img_bin, 5)
-        img_filter = img.copy()
-        img_filter[img_blur == 0] = 0
+for img_l, background in tqdm(zip(img_list, bg_list)):
+    # './train\\TRAIN_00999.jpg'
+    name = img_l[-15:]
+    img = cv2.imread(img_l)
 
-        retval, labels, stats, centroids = cv2.connectedComponentsWithStats(img_blur)
-        stats = np.array(stats)
-        stats[0, -1] = 0
-        label_idx = np.argmax(stats[:, -1])
-        img_mask = labels == label_idx
-        img_final = img_filter.copy()
-        img_final[~img_mask] = (0, 0, 0)
+    thresh = 230
+    
+    img_bin = np.array(img[:, :, 0] < thresh, dtype=np.uint8) * 255
+    img_blur = cv2.medianBlur(img_bin, 5)
+    img_filter = img.copy()
+    img_filter[img_blur == 0] = 0
 
-        img_back = cv2.resize(cv2.imread(background), img.shape[::-1][1:])
-        img_back[img_mask] = (0, 0, 0)
-        img_filter += img_back
+    retval, labels, stats, centroids = cv2.connectedComponentsWithStats(img_blur)
+    stats = np.array(stats)
+    stats[0, -1] = 0
+    label_idx = np.argmax(stats[:, -1])
+    img_mask = labels == label_idx
+    img_final = img_filter.copy()
+    img_final[~img_mask] = (0, 0, 0)
 
-        cv2.imwrite(f"../data/merge_train/{name}",img_filter)
+    img_back = cv2.resize(cv2.imread(background), img.shape[::-1][1:])
+    img_back[img_mask] = (0, 0, 0)
+    img_filter += img_back
+
+    with open('./name.txt', 'a') as f:
+        f.write(f'{name}')
+    cv2.imwrite(f"../data/merge_train/{name}",img_filter)
